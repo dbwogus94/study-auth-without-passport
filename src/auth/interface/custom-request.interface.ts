7a78de9627a3330af9d2ff24bb7abf6a75f85aeb
Yoo jae hyun <57210007+dbwogus94@users.noreply.github.com>
2022-11-21 00:49:06 +0900

    Revert "Feature/#1 auth api (#1) (#2)"
    This reverts commit de6e31cad0a9b17bf0b74ce5d5f9cc5b3f542b11.

--- src/auth/interface/custom-request.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from 'express';
-
-export type JwtPayload = { sub: string };
-
-export interface CustomRequest extends Request {
-  headers: Record<string, string | string[]>;
-  user?: JwtPayload;
-}
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ACCESS_TOKEN_TAG } from './common/constant/auth.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Auth Test API')
-    .setVersion('1.0.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'Access JWT',
-        description: 'Enter Access Token',
-        in: 'header',
-      },
-      ACCESS_TOKEN_TAG,
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('/docs', app, document);
-
   await app.listen(3000);
 }
 bootstrap();
