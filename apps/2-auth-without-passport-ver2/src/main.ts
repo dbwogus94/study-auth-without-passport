8c4f84b6a31304e395929a160178fb283093e3f8
jae hyun <1994dbwogus@gmail.com>
2022-12-20 19:28:33 +0900

    feat(app): main.ts 초기 설정과 코드 모듈화
    - bootServer: nest 앱 빌드 및 AppModule 초기화
    - preInitServer: 미들웨어 설정 및 swagger docs 빌드
    - initServer: 빌드완료된 app에 전역 파이프 설정 및 port 리스닝

--- apps/2-auth-without-passport-ver2/src/main.ts
+++ apps/2-auth-without-passport-ver2/src/main.ts
@@ -1,8 +1,37 @@
+import { buildSwagger } from '@lib/common';
+import { validationPipeOptions } from '@lib/common/option/validation-pipe.options';
+import { SwaggerConfig } from '@lib/config';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await bootServer();
+  const configService = app.get(ConfigService);
+  preInitServer(app, configService);
+  await initServer(app, configService);
 }
 bootstrap();
+
+async function bootServer(): Promise<INestApplication> {
+  const appName = process.env.APP_NAME ?? 'Nest APP';
+  return await NestFactory.create<INestApplication>(AppModule, {
+    logger: new Logger(appName),
+  });
+}
+
+function preInitServer(app: INestApplication, config: ConfigService): void {
+  const { docsOption } = config.get<SwaggerConfig>('swagger');
+  app.use(helmet());
+  app.enableCors({ origin: '*' });
+  buildSwagger('/docs', app, docsOption);
+}
+
+async function initServer(app: INestApplication, config: ConfigService) {
+  await app
+    .useGlobalPipes(new ValidationPipe(validationPipeOptions))
+    .setGlobalPrefix('/api')
+    .listen(config.get('port'));
+}
