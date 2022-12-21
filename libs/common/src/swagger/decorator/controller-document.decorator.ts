f95520ef59756d31d2fb4b6da5092bad773bd6bf
jae hyun <1994dbwogus@gmail.com>
2022-12-21 15:30:33 +0900

    feat(lib): ApiControllerDocument에 하위 데코레이터 추가

--- libs/common/src/swagger/decorator/controller-document.decorator.ts
+++ libs/common/src/swagger/decorator/controller-document.decorator.ts
@@ -3,6 +3,7 @@ import {
   ApiBadRequestResponse,
   ApiNotFoundResponse,
   ApiTags,
+  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 
 export const ApiControllerDocument = (apiTag: string) => {
@@ -12,6 +13,9 @@ export const ApiControllerDocument = (apiTag: string) => {
     }),
     ApiNotFoundResponse({
       description: '요청한 자원이 존재하지 않거나 사용할 수 없습니다.',
+    }),
+    ApiUnauthorizedResponse({
+      description: 'JWT가 만료되었거나 잘못되었습니다.',
     }),
     ApiTags(apiTag),
   );
