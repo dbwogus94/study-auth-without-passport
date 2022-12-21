aff45917f00d349ff3be245ed9bbbff5de835f40
jae hyun <1994dbwogus@gmail.com>
2022-12-21 15:30:21 +0900

    fix: main 셋팅 버그 수정
    - app.setGlobalPrefix설정 Swagger 빌드 이전에 하기 위해 순서 변경
    - Nest app 생성시 주입하는 로거 제거
    - 함수 선언 순서 변경

--- apps/2-auth-without-passport-ver2/src/main.ts
+++ apps/2-auth-without-passport-ver2/src/main.ts
@@ -1,11 +1,29 @@
 import { buildSwagger } from '@lib/common';
 import { validationPipeOptions } from '@lib/common/option/validation-pipe.options';
 import { SwaggerConfig } from '@lib/config';
-import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { AppModule } from './app.module';
+
+async function bootServer(): Promise<INestApplication> {
+  return await NestFactory.create<INestApplication>(AppModule);
+}
+
+function preInitServer(app: INestApplication, config: ConfigService): void {
+  const { docsOption } = config.get<SwaggerConfig>('swagger');
+  app.use(helmet());
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('/api');
+  buildSwagger('/docs', app, docsOption);
+}
+
+async function initServer(app: INestApplication, config: ConfigService) {
+  await app
+    .useGlobalPipes(new ValidationPipe(validationPipeOptions))
+    .listen(config.get('port'));
+}
 
 async function bootstrap() {
   const app = await bootServer();
@@ -14,24 +32,3 @@ async function bootstrap() {
   await initServer(app, configService);
 }
 bootstrap();
-
-async function bootServer(): Promise<INestApplication> {
-  const appName = process.env.APP_NAME ?? 'Nest APP';
-  return await NestFactory.create<INestApplication>(AppModule, {
-    logger: new Logger(appName),
-  });
-}
-
-function preInitServer(app: INestApplication, config: ConfigService): void {
-  const { docsOption } = config.get<SwaggerConfig>('swagger');
-  app.use(helmet());
-  app.enableCors({ origin: '*' });
-  buildSwagger('/docs', app, docsOption);
-}
-
-async function initServer(app: INestApplication, config: ConfigService) {
-  await app
-    .useGlobalPipes(new ValidationPipe(validationPipeOptions))
-    .setGlobalPrefix('/api')
-    .listen(config.get('port'));
-}
